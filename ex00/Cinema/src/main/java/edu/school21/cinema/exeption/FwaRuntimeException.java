2be545bcb8f03d3fac2ac72e6252f3fbbebd1555
achanel <achanel@student.21-school.ru>
2022-10-22 21:39:52 +0300

    Day 3

---
+++ ex00/Cinema/src/main/java/edu/school21/cinema/exeption/FwaRuntimeException.java
@@ -0,0 +1,48 @@
+package main.java.edu.school21.cinema.exeption;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FwaRuntimeException extends RuntimeException{
+    private final Map<String, String> errors;
+
+    public FwaRuntimeException() {
+        errors = new HashMap<>();
+    }
+
+    public FwaRuntimeException(Map<String, String> errors) {
+        this.errors = errors;
+    }
+
+    public FwaRuntimeException(String message) {
+        super(message);
+        this.errors = new HashMap<>();
+        this.errors.put("commonError", message);
+    }
+
+    public FwaRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+        this.errors = new HashMap<>();
+        this.errors.put("commonError", message);
+    }
+
+    public FwaRuntimeException(Throwable cause) {
+        super(cause);
+        this.errors = new HashMap<>();
+    }
+
+    public FwaRuntimeException(Map<String, String> errors, Throwable cause) {
+        super(cause);
+        this.errors = errors;
+    }
+
+    public FwaRuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+        this.errors = new HashMap<>();
+        this.errors.put("commonError", message);
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+}
